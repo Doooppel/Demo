80241297dea63833e1e2850d14741c3821c037b6
Doppelle <784397720@qq.com>
2022-06-26 15:06:34 +0800

    update package

--- main/java/com/doppel/test/DemoMain.java
+++ main/java/com/doppel/test/DemoMain.java
@@ -1,4 +1,4 @@
-package com.doppel;
+package com.doppel.test;
 
 import lombok.Builder;
 
--- test/java/com/doppel/DemoMainTest.java
+++ test/java/com/doppel/DemoMainTest.java
@@ -1,5 +1,6 @@
 package com.doppel;
 
+import com.doppel.test.DemoMain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
